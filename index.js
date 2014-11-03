f4f1f9013e475f80cae187c5af999c3746aaa018
Pierre Inglebert <pierre.inglebert@gmail.com>
2014-11-03 07:13:36 +0100

    add stderr & stdout to err object

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ var exec = require('child_process').exec;
 module.exports = function(cmd, opts){
   return function(done){
     exec(cmd, opts, function(err, stdout, stderr){
+      if(err) {
+        err.stdout = stdout;
+        err.stderr = stderr;
+      }
       done(err, stdout);
     });
   }
--- test/index.js
+++ test/index.js
@@ -1,25 +1,23 @@
 
 var co = require('co');
 var exec = require('..');
-var assert = require('assert');
+var should = require('should');
 
 describe('exec(cmd)', function(){
   it('should return stdout', function(done){
     co(function *(){
       var ret = yield exec('echo hello');
       ret.should.equal('hello\n');
-      done();
-    });
+    })(done);
   })
 
   it('should throw on error', function(done){
     co(function *(){
-      try {
-        var ret = yield exec('does_not_exist');
-      } catch (err) {
-        err.message.should.include('does_not_exist');
-        done();
-      }
+      var ret = yield exec('does_not_exist');
+    })(function(err) {
+      err.should.have.properties('stderr', 'stdout');
+      err.message.should.containEql('does_not_exist');
+      done();
     });
   })
 })
@@ -29,7 +27,6 @@ describe('exec(cmd, opts)', function(){
     co(function *(){
       var ret = yield exec('echo hello', { encoding: 'base64' });
       new Buffer(ret, 'base64').toString().should.equal('hello\n');
-      done();
-    });
+    })(done);
   })
 })
